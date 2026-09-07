23afc8cfa76971a172e60543490829028d01b9f3
chief-priest <chiefpriest0412@yahoo.com>
2026-09-07 13:53:58 +0000

    feat: upgrade frontend to 1.1.0

--- services/frontend/app.py
+++ services/frontend/app.py
@@ -1,10 +1,21 @@
 from flask import Flask
 import os
+import requests
 
 app = Flask(__name__)
 
 VERSION = os.getenv("APP_VERSION", "1.0.0")
 ENVIRONMENT = os.getenv("ENVIRONMENT", "dev")
+
+PRODUCT_SERVICE_URL = os.getenv(
+    "PRODUCT_SERVICE_URL",
+    "http://product-service:8081"
+)
+
+ORDER_SERVICE_URL = os.getenv(
+    "ORDER_SERVICE_URL",
+    "http://order-service:8082"
+)
 
 
 @app.route("/")
@@ -15,6 +26,26 @@ def home():
         "environment": ENVIRONMENT,
         "message": "Welcome to the Dev-to-Prod Promotion Demo"
     }
+
+
+@app.route("/products")
+def products():
+    response = requests.get(
+        f"{PRODUCT_SERVICE_URL}/",
+        timeout=5
+    )
+
+    return response.json(), response.status_code
+
+
+@app.route("/orders")
+def orders():
+    response = requests.get(
+        f"{ORDER_SERVICE_URL}/orders",
+        timeout=5
+    )
+
+    return response.json(), response.status_code
 
 
 @app.route("/health")
